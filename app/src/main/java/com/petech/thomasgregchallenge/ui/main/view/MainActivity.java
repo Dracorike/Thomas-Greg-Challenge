51ffe3e1ce71b38f8f6e3644ec1f787521e760f3
Dracorike <pedrohenriqueknd@gmail.com>
2024-01-11 16:05:43 -0300

    Starting main activity.

--- app/src/main/java/com/petech/thomasgregchallenge/ui/main/view/MainActivity.java
+++ app/src/main/java/com/petech/thomasgregchallenge/ui/main/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.petech.thomasgregchallenge;
+package com.petech.thomasgregchallenge.ui.main.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.petech.thomasgregchallenge.R;
 
 public class MainActivity extends AppCompatActivity {
 
